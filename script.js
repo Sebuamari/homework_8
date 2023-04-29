189e42d712fe36c12fb251a57e5cfc718d6f032e
Mariam Sebua <70912461+Sebuamari@users.noreply.github.com>
2023-04-29 11:25:03 +0400

    feat: finished

--- script.js
+++ script.js
@@ -34,3 +34,61 @@ for(let i = 0; i < array4.length; i++){
 }
 let average = sumOfArray4 / array4.length;
 console.log(average);
+
+// 6
+let array5 = [1, 2, 3, 7, 6, 9];
+
+for ( num of array5 ) {
+    num !== 7 ? console.log(num) : '';
+}
+
+// 7
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus: "active"
+};
+  
+console.log(user.studentstatus);
+
+// 8 
+let user1 = {
+    name: 'giorgi',
+    age:  20,
+    studentstatus: 'active'
+}
+
+if ( user1.studentstatus === 'active' || user1.age < 25 ) {
+    if ( user1.age < 18 && user1.studentstatus === 'active' ) {
+        if ( user1.name === 'Levani' ) {
+            console.log("Hello Levani");
+        } else {
+            console.log("Hello");
+        }
+    } else {
+        console.log("Hello World");
+    }
+} else {
+    console.log("error")
+}
+
+// 9
+let array6 = [
+    [2, -3, 5, 10],
+    [25, -24, -11, 100],
+    [-6, -7, 10],
+];
+
+for ( let  i = 0; i < array6.length; i++ ) {
+    for( let j = 0; j < array6[i].length; j++ ) {
+        array6[i][j] > 0 ? console.log(array6[i][j]) : ''
+    }
+}
+
+// 10
+let array7 = [ 2, 3, 5, 10, 25, 24, 11, 100, 6, 7, 10 ];
+let oddNumbers = array7.filter(num => num % 2 !== 0 );
+let evenNumbers = array7.filter(num => num % 2 === 0 );
+console.log(oddNumbers);
+console.log(evenNumbers);
